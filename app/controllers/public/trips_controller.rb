b61af038bec5776fd28f878c489e7a23e3aaaf15
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-09-04 13:08:29 +0000

    Update Admin_user_edit

--- app/controllers/public/trips_controller.rb
+++ app/controllers/public/trips_controller.rb
@@ -1,13 +1,17 @@
 class Public::TripsController < ApplicationController
   def new
+    @trip = Trip.new
   end
 
   def index
+    @trips = Trips.all
   end
 
   def show
+    @trip = Trip.find(params[:id])
   end
 
   def edit
+    @trip = Trip.find(params[:id])
   end
 end
