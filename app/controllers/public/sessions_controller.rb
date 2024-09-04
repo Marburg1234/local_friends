1e522e338676115bcf4f8a23bcf100ee7c0a74a8
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-09-04 06:16:47 +0000

    Update withdraw_function

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :user_state, only: [:create]
 
   def after_sign_in_path_for(resource)
     user_path(current_user)
@@ -11,27 +9,22 @@ class Public::SessionsController < Devise::SessionsController
     root_path
   end
 
+  # 退会したユーザーがログインできないようにする
+  private
+  # アクティブであるかを判断するメソッド
+  def user_state
+    # 【処理内容1】 入力されたemailからアカウントを1件取得
+    user = User.find_by(email: params[:user][:email])
+    # 【処理内容2】 アカウントを取得できなかった場合、このメソッドを終了する
+    return if user.nil?
+    # 【処理内容3】 取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了する
+    return unless user.valid_password?(params[:user][:password])
+     flash[:notice] = "項目を入力してください"
 
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    # 【処理内容4】 アクティブでない会員に対する処理
+    if user.is_active == false
+      flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
+      redirect_to new_user_registration_path
+    end
+  end
 end
