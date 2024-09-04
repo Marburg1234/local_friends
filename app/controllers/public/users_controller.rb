13e1ca02f5db8c630eb963aba7e826b486634c07
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-09-04 02:41:46 +0000

    Finish user_edit & update

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,6 +3,18 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "編集内容を保存しました"
+      redirect_to user_path(@user)
+    else
+      flash[:notice] = "編集に失敗しました"
+      render :edit
+    end
   end
 
   def show
@@ -16,4 +28,12 @@ class Public::UsersController < ApplicationController
 
   def unsubscribe
   end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :family_name, :introduction, :topic, :profile_image, :sub_image, :practice_language_id)
+  end
+
 end
