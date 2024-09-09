ce101ac5ead058a4e2204a1901fbc55d3d48393d
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-09-09 10:34:31 +0000

    Modified admin_user_update_redirect_to

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -18,7 +18,7 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "編集内容を保存しました"
-      redirect_to admin_users_path
+      redirect_to admin_user_path
     else
       @user = User.find(params[:id])
       flash[:notice] = "編集に失敗しました"
