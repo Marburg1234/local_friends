6a52c45848625ac2bbeed31dde44d7cbc68a7136
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-09-10 08:12:35 +0000

    Add Ajax_comment

--- app/controllers/public/trip_comments_controller.rb
+++ app/controllers/public/trip_comments_controller.rb
@@ -1,18 +1,18 @@
 class Public::TripCommentsController < ApplicationController
 
   def create
-    trip = Trip.find(params[:trip_id])
-    comment = current_user.trip_comments.new(trip_comment_params)
-    comment.trip_id = trip.id
-    comment.save
-    redirect_to request.referer
+    @trip = Trip.find(params[:trip_id])
+    @comment = current_user.trip_comments.new(trip_comment_params)
+    @comment.trip_id = @trip.id
+    @comment.save
+    # redirect_to request.referer
   end
 
   def destroy
-    comment = TripComment.find(params[:id])
-    comment.destroy
-    @trip = Trip.find(params[:book_id])
-    redirect_to request.referer
+    @comment = TripComment.find(params[:id])
+    @comment.destroy
+    @trip = Trip.find(params[:trip_id])
+    # redirect_to request.referer
   end
 
   private
