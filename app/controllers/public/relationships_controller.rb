2ecb9a4f5640482b31ef2f535d8eb9387e3c1460
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-09-14 10:33:55 +0000

    Update Ajax_follow_function

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,14 +2,16 @@ class Public::RelationshipsController < ApplicationController
 
   def create
     # follow = current_user.active_relationships.build(followed_id: params[:user_id])
-    follow = current_user.active_relationships.build(followed_id: params[:user_id])
-    follow.save
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    @follow = current_user.active_relationships.build(followed_id: params[:user_id])
+    @follow.save
+    # redirect_to request.referer
   end
 
   def destroy
-    follow = current_user.active_relationships.find_by(followed_id: params[:user_id])
-    follow.destroy
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    @follow = current_user.active_relationships.find_by(followed_id: params[:user_id])
+    @follow.destroy
+    # redirect_to request.referer
   end
 end
